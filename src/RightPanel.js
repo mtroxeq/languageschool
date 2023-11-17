85b7416b4b0bb566f65a702c51d657c61e4d5ec1
mtroxeq <matroxszabat@gmail.com>
2023-11-17 09:32:43 +0100

    Dodano rejestracje

--- src/RightPanel.js
+++ src/RightPanel.js
@@ -1,8 +1,10 @@
+import { Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route} from "react-router-dom";
+import Registration from "./Registration";
+
 function RightPanel(){
     return(
-        <article className='RightPanel'>
-
-        </article>
+        
     );
 }
 
